4f9a46affbefb2882fd69401e928128a84805dfc
ricosmall <805163443@qq.com>
2020-03-24 22:07:56 +0800

    test: add stack test

---
+++ test/stack.test.js
@@ -0,0 +1,103 @@
+import Stack from '../src/stack'
+
+describe('Stack', () => {
+  let stack
+  beforeEach(() => {
+    stack = new Stack()
+  })
+
+  test('create a instance to equal blank object', () => {
+    expect(stack.items).toEqual({})
+  })
+
+  test('push elements', () => {
+    stack.push(1)
+    expect(stack.size()).toBe(1)
+    stack.push(2)
+    expect(stack.size()).toBe(2)
+    stack.push(3)
+    expect(stack.size()).toBe(3)
+
+    expect(stack.isEmpty()).toBe(false)
+  })
+
+  test('pop elements', () => {
+    stack.push(1)
+    stack.push(2)
+    stack.push(3)
+
+    expect(stack.pop()).toBe(3)
+    expect(stack.size()).toBe(2)
+    expect(stack.pop()).toBe(2)
+    expect(stack.size()).toBe(1)
+    expect(stack.pop()).toBe(1)
+    expect(stack.isEmpty()).toBe(true)
+    expect(stack.pop()).toBe(undefined)
+    expect(stack.isEmpty()).toBe(true)
+  })
+
+  test('implements LIFO logic', () => {
+    stack.push(1)
+    stack.push(2)
+    stack.push(3)
+
+    expect(stack.pop()).toBe(3)
+    expect(stack.pop()).toBe(2)
+    expect(stack.pop()).toBe(1)
+    expect(stack.pop()).toBe(undefined)
+  })
+
+  test('peek element', () => {
+    stack.push(1)
+    stack.push(2)
+
+    expect(stack.size()).toBe(2)
+    expect(stack.peek()).toBe(2)
+    expect(stack.size()).toBe(2)
+    
+    stack.push(3)
+    expect(stack.peek()).toBe(3)
+    expect(stack.size()).toBe(3)
+  })
+
+  test('return correct sizes', () => {
+    expect(stack.size()).toBe(0)
+    stack.push(1)
+    expect(stack.size()).toBe(1)
+    stack.push(2)
+    expect(stack.size()).toBe(2)
+    stack.pop()
+    expect(stack.size()).toBe(1)
+    stack.clear()
+    expect(stack.size()).toBe(0)
+  })
+
+  test('return if it is empty', () => {
+    expect(stack.isEmpty()).toBe(true)
+    stack.push(1)
+    stack.push(2)
+    expect(stack.isEmpty()).toBe(false)
+
+    stack.pop()
+    expect(stack.isEmpty()).toBe(false)
+    stack.clear()
+    expect(stack.isEmpty()).toBe(true)
+  })
+
+  test('remove all elements', () => {
+    stack.push(1)
+    stack.push(2)
+
+    expect(stack.size()).toBe(2)
+    stack.clear()
+    expect(stack.isEmpty()).toBe(true)
+  })
+
+  test('toString', () => {
+    expect(stack.toString()).toBe('')
+    stack.push(1)
+    expect(stack.toString()).toBe('1')
+    stack.push(2)
+    expect(stack.toString()).toBe('1,2')
+  })
+})

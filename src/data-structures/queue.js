8da8d06c43012a1a8a2158772ed37fbca5377fab
ricosmall <805163443@qq.com>
2020-03-26 23:04:46 +0800

    feat: add queue data structure

---
+++ src/data-structures/queue.js
@@ -0,0 +1,54 @@
+export default class Queue {
+  constructor () {
+    this.items = {}
+    this.count = 0
+    this.lowestCount = 0
+  }
+
+  enqueue (element) {
+    this.items[this.count] = element
+    this.count += 1
+  }
+
+  dequeue () {
+    if (this.isEmpty()) {
+      return undefined
+    }
+    const result = this.items[this.lowestCount]
+    delete this.items[this.lowestCount]
+    this.lowestCount += 1
+    return result
+  }
+
+  peek () {
+    if (this.isEmpty()) {
+      return undefined
+    }
+    return this.items[this.lowestCount]
+  }
+
+  size () {
+    return this.count - this.lowestCount
+  }
+
+  isEmpty () {
+    return this.size() === 0
+  }
+
+  clear () {
+    this.items = {}
+    this.count = 0
+    this.lowestCount = 0
+  }
+
+  toString () {
+    if (this.isEmpty()) {
+      return ''
+    }
+    let result = `${this.items[this.lowestCount]}`
+    for (let i = this.lowestCount + 1; i < this.count; i++) {
+      result += `,${this.items[i]}`
+    }
+    return result
+  }
+}

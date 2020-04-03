323fa1d8a4b1d37029b65d42c613d210829565c2
ricosmall <805163443@qq.com>
2020-04-03 23:54:39 +0800

    test: add circular linked list data structure test

---
+++ test/circular-linked-list.test.js
@@ -0,0 +1,39 @@
+import CircularLinkedList from '../src/data-structures/circular-linked-list'
+
+describe('CircularLinkedList', () => {
+  let circularLinkedList
+  beforeEach(() => {
+    circularLinkedList = new CircularLinkedList()
+  })
+
+  test('push elements', () => {
+    expect(circularLinkedList.size()).toBe(0)
+    circularLinkedList.push(1)
+    circularLinkedList.push(2)
+    circularLinkedList.push(3)
+    expect(circularLinkedList.size()).toBe(3)
+  })
+
+  test('insert elements', () => {
+    circularLinkedList.push(1)
+    circularLinkedList.push(2)
+    circularLinkedList.insert(3, 1)
+    expect(circularLinkedList.getElementAt(1).element).toBe(3)
+    expect(circularLinkedList.size()).toBe(3)
+    circularLinkedList.insert(4, 0)
+    expect(circularLinkedList.head.element).toBe(4)
+    expect(circularLinkedList.size()).toBe(4)
+    circularLinkedList.insert(5, 4)
+    expect(circularLinkedList.getElementAt(circularLinkedList.size() - 1).element).toBe(5)
+  })
+
+  test('remove elements', () => {
+    circularLinkedList.push(1)
+    circularLinkedList.push(2)
+    circularLinkedList.push(3)
+    expect(circularLinkedList.removeAt(0)).toBe(1)
+    expect(circularLinkedList.size()).toBe(2)
+    expect(circularLinkedList.removeAt(1)).toBe(3)
+    expect(circularLinkedList.size()).toBe(1)
+  })
+})

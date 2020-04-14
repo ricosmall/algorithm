eb78444b09476a1e63b7c43f491f64127c0892bf
ricosmall <805163443@qq.com>
2020-04-14 23:07:04 +0800

    feat: add fibonacci algorithm

---
+++ src/algorithms/fibonacci.js
@@ -0,0 +1,18 @@
+export function fibonacciIteration(n) {
+  if (n < 1) return 0
+  let n1 = 0
+  let n2 = 1
+  let res = n1 + n2
+  for (let i = 2; i < n; i++) {
+    n1 = n2
+    n2 = res
+    res = n2 + n1
+  }
+  return res
+}
+
+export function fibonacci(n) {
+  if (n < 1) return 0
+  if (n <= 2) return 1
+  return fibonacci(n - 1) + fibonacci(n - 2)
+}
